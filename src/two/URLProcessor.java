bd96125098aaaba065b7c253c9cc5f573d9288ba
Evangelia <eva@PurpleChimera.(none)>
2011-03-20 16:33:13 -0400

    Change to output

--- src/two/URLProcessor.java
+++ src/two/URLProcessor.java
@@ -17,7 +17,7 @@ public class URLProcessor {
 	        try {
 	        	
 	        	/* change to /usr/local/bin/lynx" for clic machines */
-	            String cmdline[] = {"/usr/local/bin/lynx", "--dump", url };
+	            String cmdline[] = {"/usr/bin/lynx", "--dump", url };
 	            Process p = Runtime.getRuntime().exec(cmdline);
 	            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
 	            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
--- src/two/YahooProber.java
+++ src/two/YahooProber.java
@@ -34,7 +34,7 @@ class DocumentSample {
 }
 
 /**
- * maintained for each URL / document
+ * Maintained for each URL / document
  */
 class Document {
 	String url;
@@ -55,9 +55,9 @@ public class YahooProber {
 	// Number of hierarchy levels
 	private static int levels = 2;
 	// Default url
-	private String databaseURL = "www.afaa.com";
+	private String databaseURL = "hardwarecentral.com";
 	// Default specificity
-	private double SPECIFICITY = 0.6;
+	private double SPECIFICITY = 0.3;
 	// Default coverage
 	private int COVERAGE = 100;
 	// Number of top results to use for content-summary
@@ -66,13 +66,12 @@ public class YahooProber {
 	private HashMap<String, String> catNodes = new HashMap<String, String>();
 	// Cached results for queries
 	private HashMap<String, Set<String>> cachedResults = new HashMap<String, Set<String>>();
-
-	// Result for categories
+	// Results for categories
 	private HashMap<String, Double> catSpecificity = new HashMap<String, Double>();
 	private HashMap<String, Integer> catCoverage = new HashMap<String, Integer>();
 	private HashMap<String, Integer> overallCoverage = new HashMap<String, Integer>();
 
-	/*
+	/**
 	 * Default constructor
 	 */
 	public YahooProber() {
@@ -80,7 +79,7 @@ public class YahooProber {
 		this.classifyDB();
 	}
 
-	/*
+	/**
 	 * Constructor with arguments
 	 */
 	public YahooProber(String url, double specificity, int coverage) {
@@ -91,9 +90,9 @@ public class YahooProber {
 		this.classifyDB();
 	}
 
-	/*	
-    * Initialize category tree
-    */
+	/**
+	 * Initialize category tree
+	 */
 	public void initalizeTree() {
 		this.catNodes.put("Sports", "Root");
 		this.catNodes.put("Health", "Root");
@@ -106,35 +105,46 @@ public class YahooProber {
 		this.catNodes.put("Programming", "Computers");
 	}
 
+	/*
+	 * Getters for class members
+	 */
 	public String getParent(String catNode) {
 		return this.catNodes.get(catNode);
 	}
 
 	public int getOverallCoverage(String catNode) {
-
 		return this.overallCoverage.get(catNode);
 	}
 
 	public int getCoverage(String catNode) {
-
 		return this.catCoverage.get(catNode);
 	}
+	
+	public boolean hasChild(String cat){
+		
+		for(String c:this.catNodes.keySet()){
+			if(this.getParent(c).equals(cat) && this.catSpecificity.containsKey(c))
+				return true;
+		}
+		return false;
+	}
 
+	/**
+	 * Categorizes the database
+	 */
 	public LinkedList<String> classifyDB() {
 
 		HashSet<String> categories = new HashSet<String>();
 		categories.add("queries/Root.txt");
 		int level = 1;
-		System.out.println("Classifying ... \n\n");
+		System.out.println("Classifying ...");
 		do {
 			// for storing child categories while iterating
 			HashSet<String> tempCats = new HashSet<String>();
 			for (String cat : categories) {
 				String catName = cat.split("queries\\/|.txt")[1];
-				System.err.println(cat);
 				Set<String> docs = new HashSet<String>();
 				cachedResults.put(catName, docs);
-				System.out.println("Put "+catName);
 				int coverage = 0;
 				try {
 					FileInputStream fstream = new FileInputStream(cat);
@@ -154,17 +164,18 @@ public class YahooProber {
 						tempCats.add("queries/" + queryTerms[0] + ".txt");
 						int numhits = this.poseQuery(queryTerms, docs);
 						// Try to avoid abusing the site
-						 Thread.sleep(3000);
+						Thread.sleep(3000);
 						coverage += numhits;
 						cCoverage += numhits;
 						previousCategory = queryTerms[0];
 					}
 					this.catCoverage.put(previousCategory, cCoverage);
 					in.close();
+					// Compute the overall coverage of this category
 					this.overallCoverage.put(catName, coverage);
 					if (this.getParent(catName) != null) {
 						double specificity = this.getCoverage(catName)
-								* 1.0f
+								* 1.0
 								/ this.getOverallCoverage(this
 										.getParent(catName));
 						this.catSpecificity.put(catName, specificity);
@@ -179,19 +190,23 @@ public class YahooProber {
 			categories.addAll(tempCats);
 			tempCats.clear();
 			LinkedList<String> toRemove = new LinkedList<String>();
+			// Check the specificity and coverage of each category
 			for (String catName : categories) {
 				String c = catName.split("queries\\/|.txt")[1];
 				double specificity = this.getCoverage(c) * 1.0
 						/ this.overallCoverage.get(this.getParent(c));
 				if (this.getCoverage(c) < this.COVERAGE
 						|| specificity < this.SPECIFICITY) {
-					System.err.println("Mark to remove " + c + " "
-							+ specificity + " " + this.getCoverage(c));
 					toRemove.add(catName);
 				}
+				else
+				{
+					System.out.println("Coverage for category:"+c+" is "+this.getCoverage(c));
+					System.out.println("Specificity for category:"+c+" is "+specificity);
+				}
 			}
-
-			// remove all categories which are < Ts or Tc
+			System.out.println("\n");
+			// Remove all categories which are < Ts or Tc
 			for (String tr : toRemove) {
 				String c = tr.split("queries\\/|.txt")[1];
 				categories.remove(tr);
@@ -207,24 +222,36 @@ public class YahooProber {
 			this.catSpecificity.put(c, specificity);
 		}
 
-		/* classified categories */
+		// Classified categories
+		System.out.println("Classification:");
 		for (String fc : this.catSpecificity.keySet()) {
-			System.err.println("sc " + fc + " " + this.catSpecificity.get(fc)
-					+ " " + this.catCoverage.get(fc));
+			if(this.hasChild(fc))
+				continue;
+			String fullCat=new String(fc);
+			String p=this.getParent(fc);
+			//Print full path for classified categories
+			while(p!=null) {
+				fullCat=p+"/"+fullCat;
+				p=this.getParent(p);
+			}
+			System.out.println(fullCat);
 		}
-
-		// build content summary
+		System.out.println("\n");
+		// build content summaries
 		this.buildContentSummary();
 		return null;
 	}
 
+	/**
+	 * Build content summaries
+	 */
 	public void buildContentSummary() {
 
 		/* document sampling after category selection */
 		List<DocumentSample> docSample = new ArrayList<DocumentSample>();
 		DocumentSample dc;
 		List<String> childList = new ArrayList<String>();
-		// classified category will always be some leaf
+		// For all classified categories
 		for (String fc : this.catSpecificity.keySet()) {
 			dc = new DocumentSample();
 			// category for this document sample
@@ -241,7 +268,8 @@ public class YahooProber {
 				dc.filteredURLs = cachedResults.get(parent);
 				// add all the child documents
 				for (String c : childList) {
-					if(cachedResults.get(c)==null)
+					// Leaf categories do not have samples
+					if (cachedResults.get(c) == null)
 						continue;
 					dc.filteredURLs.addAll(cachedResults.get(c));
 				}
@@ -256,11 +284,12 @@ public class YahooProber {
 		// fetch all the webpages for each doc sample
 		for (DocumentSample ds : docSample) {
 			int docId = 0;
-			if(ds.filteredURLs==null)
+			// Skip leaf categories
+			if (ds.filteredURLs == null)
 				continue;
 			System.out.println("Building document sample for " + ds.category);
 			for (String url : ds.filteredURLs) {
-				System.out.printf("[%d] Getting page: %s \n",(docId+1),url);
+				System.out.printf("[%d] Getting page: %s \n", (docId + 1), url);
 				Set<String> words = URLProcessor.runLynx(url);
 				Document d = new Document(url);
 				d.words = words;
@@ -269,14 +298,15 @@ public class YahooProber {
 				docId++;
 			}
 
-			String fileName = "sample-"+ds.category+"-"+this.databaseURL+".txt";
+			String fileName = "sample-" + ds.category + "-" + this.databaseURL
+					+ ".txt";
 			// display result
 			this.display(ds, fileName);
 		}
 		System.out.println("\n\nFinished generating document samples..");
 	}
 
-	/*
+	/**
 	 * Display the word-document matrix
 	 */
 	public void display(DocumentSample ds, String fileName) {
@@ -289,11 +319,11 @@ public class YahooProber {
 			out.write("##############################\n");
 			int docCount = 0;
 			for (String word : ds.wordSet) {
-				out.write(word + " - ");
+				out.write(word + " # ");
 				for (String url : ds.filteredURLs) {
 					Document d = ds.urlMap.get(url);
-					if(d.words.contains(word))
-							docCount++;
+					if (d.words.contains(word))
+						docCount++;
 				}
 				out.write(docCount + "\n");
 				docCount = 0;
@@ -306,21 +336,21 @@ public class YahooProber {
 	}
 
 	/**
+	 * Poses query using Yahoo BOSS
 	 * 
 	 * @param queryTerms
 	 * @param categoryName
-	 * @return
+	 * @return number of matches for the query
 	 */
 	public int poseQuery(String[] queryTerms, Set<String> docs) {
 
 		String query = new String();
 		JSONArray ja = new JSONArray();
-		// Convert spaces to + to make a valid URL
+		//Convert spaces to + to make a valid URL
 		try {
 			for (int i = 1; i < queryTerms.length; i++)
 				query += " " + queryTerms[i];
 			query = URLEncoder.encode(query, "UTF-8");
-			//System.out.println("Probing Query: " + query);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
@@ -328,8 +358,9 @@ public class YahooProber {
 		URL url;
 		try {
 			url = new URL("http://boss.yahooapis.com/ysearch/web/v1/" + query
-					+ "?appid=" + API_KEY + "&sites=" + this.databaseURL + "&start=0"
-					+ "&count=" + YahooProber.topResults + "&format=json");
+					+ "?appid=" + API_KEY + "&sites=" + this.databaseURL
+					+ "&start=0" + "&count=" + YahooProber.topResults
+					+ "&format=json");
 			URLConnection connection = url.openConnection();
 
 			String line;
@@ -341,6 +372,7 @@ public class YahooProber {
 			}
 			String response = builder.toString();
 			JSONObject json = new JSONObject(response);
+			//Get number of matches
 			String strHits = (String) (json.getJSONObject("ysearchresponse")
 					.get("totalhits"));
 			int numHits = Integer.parseInt(strHits);
@@ -354,13 +386,9 @@ public class YahooProber {
 				if (ja.length() == i) {
 					break;
 				}
-				// System.out.print((i + 1) + ". ");
 				j = ja.getJSONObject(i);
-				// System.out.println(j.getString("url"));
 				docs.add(j.getString("url"));
-
 			}
-			// System.err.println("Number of matches :" + query + " ="+ numHits);
 			return numHits;
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
@@ -373,6 +401,9 @@ public class YahooProber {
 		return 0;
 	}
 
+	/**
+	 * Main function initiating qprobing
+	 */
 	public static void main(String args[]) {
 		if (args.length >= 3) {
 			System.out.println("Using command line arguments...");
